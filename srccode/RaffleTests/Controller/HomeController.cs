96243bf049c15c44ee0e899f1b82ef47b930a886
tehmina-ahmed <82107262+tehmina-ahmed@users.noreply.github.com>
2021-06-23 01:59:10 +0100

    ++CI

--- srccode/RaffleTests/Controller/HomeController.cs
+++ srccode/RaffleTests/Controller/HomeController.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Merge;
+using Microsoft.AspNetCore.Mvc;
+//using NPOI.SS.Formula.Functions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace RaffleTests.Controller
 {
-    class HomeController
+    public class HomeController
     {
+        private AppSettings appSettings = new AppSettings()
+        {
+            mergedServiceURL = ""
+        }
+        [Fact]
+
+        public async void GetTest()
+        {
+            HomeController homeController = new HomeController();
+            var homeControllerResult = await homeController.Index();
+
+            Assert.NotNull(homeControllerResult);
+            Assert.IsType<ActionResult<string>>(homeControllerResult);
+
+        }
     }
 }
