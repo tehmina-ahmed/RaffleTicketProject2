8ab9aaef8641dd72b8c232f038eb35caa6ecf871
tehmina-ahmed <82107262+tehmina-ahmed@users.noreply.github.com>
2021-06-23 15:30:21 +0100

    chnages1

--- srccode/FrontEnd/Controllers/HomeController.cs
+++ srccode/FrontEnd/Controllers/HomeController.cs
@@ -8,25 +8,24 @@ using Microsoft.Extensions.Logging;
 using FrontEnd.Models;
 using System.Net.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace FrontEnd.Controllers
 {
     //mergeURL: https://localhost:44323/merge
     public class HomeController : Controller
     {
-        
-        private readonly ILogger<HomeController> _logger;
-        private IConfiguration Configuration;
 
-        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        private AppSettings Configuration;
+        public HomeController(IOptions<AppSettings> settings)
         {
-            _logger = logger;
-            Configuration = configuration;
+            Configuration = settings.Value;
         }
 
         public async Task<IActionResult> Index()
         {
-            var mergedService = "https://localhost:44323/merge";
+            //var mergedService = "https://localhost:44323/merge";
+            var mergedService = $"{Configuration.mergedServiceURL}/merge";
             var mergeResponseCall = await new HttpClient().GetStringAsync(mergedService);
             ViewBag.responseCall = mergeResponseCall;
             return View();
